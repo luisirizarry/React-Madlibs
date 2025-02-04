d2eea94399c13dbfab0bb87677ab1fa32be2aa57
Luis Irizarry <lirizarry6178@gmail.com>
2025-02-04 00:58:41 -0500

    finished add commas function

--- addCommas.test.js
+++ addCommas.test.js
@@ -5,3 +5,25 @@ describe("#addCommas", () => {
     expect(typeof addCommas).toBe("function");
   });
 });
+
+test("1234 as '1,234'", () => {
+  expect(addCommas(1234)).toBe("1,234");
+});
+
+test("1000000 as '1,000,000'", () => {
+  expect(addCommas(1000000)).toBe("1,000,000");
+});
+
+test("handles large numbers", () => {
+  expect(addCommas(123456789012345)).toBe("123,456,789,012,345");
+});
+
+test("does not add commas for single digit numbers", () => {
+  expect(addCommas(6)).toBe("6");
+});
+
+test("handles negative numbers correctly", () => {
+  expect(addCommas(-10)).toBe("-10");
+  expect(addCommas(-5678)).toBe("-5,678");
+});
+
